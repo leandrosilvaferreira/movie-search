cd82b07c11528e6b5f4736ff7c7f3649a94dcf76
Leandro Silva Ferreira <leandrosilvaferreira@gmail.com>
2018-12-16 17:17:30 -0200

    adequacoes

--- src/App.js
+++ src/App.js
@@ -1,87 +1,89 @@
-import React, { Component } from 'react';
-import logo from './images/logo.png';
-import search from './images/search.png';
-import axios from 'axios';
-import { FaSearch } from 'react-icons/fa';
-import { DebounceInput } from 'react-debounce-input';
-import Movie from './components/Movie';
-import { BarLoader } from 'react-spinners';
+import React, { Component } from "react";
+import logo from "./images/logo.png";
+import search from "./images/search.png";
+import axios from "axios";
+import { FaSearch } from "react-icons/fa";
+import { DebounceInput } from "react-debounce-input";
+import Movie from "./components/Movie";
+import { BarLoader } from "react-spinners";
 
 class App extends Component {
   state = {
-    search_value: '',
+    search_value: "",
     movies: [],
     loading: false
-  }
+  };
 
-  searchMovie = async (name) => {
+  searchMovie = async name => {
+    
     this.setState({
       loading: true
-    })
+    });
+
     await axios({
-      method: 'get',
-      url: `http://www.omdbapi.com/?s=${name}&apikey=12a14590`,
-    })
-    .then((response) => {
-      if(response.data.Error === "Movie not found!") {
+      method: "get",
+      url: `http://www.omdbapi.com/?s=${name}&apikey=12a14590`
+    }).then(response => {
+      if (response.data.Error === "Movie not found!") {
         this.setState({
           movies: [],
           loading: false
-        })
+        });
       } else {
         this.setState({
           movies: response.data.Search,
           loading: false
-        })
+        });
       }
     });
-  }
+  };
 
-  handleInputChange = async (event) => {
+  handleInputChange = async event => {
     const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
 
     await this.setState({
       search_value: value
-    })
+    });
 
-    if(value.length === 0) {
+    if (value.length === 0) {
       this.setState({
         movies: []
-      })
+      });
     } else {
       this.searchMovie(this.state.search_value);
     }
-  }
+  };
 
   renderMovies = () => {
-    console.log(this.state.movies)
-    if(this.state.movies.length > 0) {
+    
+    console.log(this.state.movies);
+
+    if (this.state.movies.length > 0) {
       return this.state.movies.map(movie => {
-        return (
-          <Movie movie={movie} />
-        )
-      })
-    } else {
+        return <Movie movie={movie} />;
+      });
+    } 
+    else {
       return (
         <div className="empty-movies">
-          <img src={search} alt=""/>
+          <img src={search} alt="" />
           <h1>Don't know what to search?</h1>
-          <p>Here's an offer you can't refuse.</p>
+          <p>Here's an offer you can't refuse: The Godfather!</p>
         </div>
-      )
+      );
     }
-  }
+  };
 
   render() {
     return (
       <div className="container mt-4">
         <div className="logo">
-          <img src={logo} alt=""/>
+          <img src={logo} alt="" />
         </div>
         <div className="input-container">
           <div className="input-icon">
-            <FaSearch color={'#9DABB5'} />
+            <FaSearch color={"#9DABB5"} />
           </div>
           <DebounceInput
             minLength={3}
@@ -93,15 +95,13 @@ class App extends Component {
           />
         </div>
         <div className="movies-container">
-          {this.state.loading ?
+          {this.state.loading ? (
             <div className="loading">
-              <BarLoader
-                color={'#FFF'}
-                height={10}
-                width={150}
-              />
+              <BarLoader color={"#FFF"} height={10} width={150} />
             </div>
-            : this.renderMovies()}
+          ) : (
+            this.renderMovies()
+          )}
         </div>
       </div>
     );
--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,37 +1,40 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { FaArrowLeft } from 'react-icons/fa';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 export default class MovieDetail extends Component {
   state = {
     movie: {}
-  }
+  };
 
   componentDidMount = async () => {
     await axios({
-      method: 'get',
-      url: `http://www.omdbapi.com/?i=${this.props.match.params.id}&apikey=12a14590`,
-    })
-      .then((response) => {
-        this.setState({ movie: { ...response.data } })
-        console.log(response.data)
-      });
-  }
+      method: "get",
+      url: `http://www.omdbapi.com/?i=${
+        this.props.match.params.id
+      }&apikey=12a14590`
+    }).then(response => {
+      this.setState({ movie: { ...response.data } });
+      console.log(response.data);
+    });
+  };
 
   render() {
     const { movie } = this.state;
     return (
       <div className="container mt-8">
-        <Link to='/'>
+        <Link to="/">
           <div className="back">
-
-          <FaArrowLeft />
+            <FaArrowLeft />
           </div>
         </Link>
         <div className="movie-detail-container">
           <div className="p-4  width-500">
-            <p><span>{movie.Runtime}</span> <span>{movie.Year}</span> <span className="rated">{movie.Rated}</span></p>
+            <p>
+              <span>{movie.Runtime}</span> <span>{movie.Year}</span>{" "}
+              <span className="rated">{movie.Rated}</span>
+            </p>
             <p className="title">{movie.Title}</p>
             <div className="flex mb-4">
               <span>{movie.imdbRating}</span>
@@ -57,11 +60,14 @@ export default class MovieDetail extends Component {
                 <p>{movie.Director}</p>
               </div>
             </div>
-
           </div>
-          <img src={movie.Poster} className="img-detail img-responsive" alt="" />
+          <img
+            src={movie.Poster}
+            className="img-detail img-responsive"
+            alt=""
+          />
         </div>
       </div>
-    )
+    );
   }
 }
